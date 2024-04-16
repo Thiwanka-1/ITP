e99ca2192ec3d12ae0533568ccda34ffb63444e3
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2024-04-16 17:04:58 +0530

    Update server.js

--- Gym_Management_System/BackEnd/server.js
+++ Gym_Management_System/BackEnd/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
 
@@ -11,39 +11,44 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 const URL = process.env.MONGODB_URL;
 
-mongoose.connect(URL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    
-});
 
+mongoose.connect(URL,{
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+});
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDb Connection Success!");
-});
+connection.once('open' , () =>{
+    console.log("Mongodb Connection success!");
 
-const workoutrouter = require("./routes/WorkoutPlans.js");
-app.use("/workoutplan",workoutrouter);
+})
 
-const Twodayworkoutrouter = require("./routes/Twodayworkoutroute.js");
-app.use("/Twodayworkoutplan",Twodayworkoutrouter);
+const AppoinmentRouter = require("./routes/AppoinmentRoutes.js");
+app.use("/appointment", AppoinmentRouter );
 
-const Threedayworkoutrouter = require("./routes/Threedayworkoutroute.js");
-app.use("/Threedayworkoutplan",Threedayworkoutrouter);
+const maintenanceRouter = require("./routes/Maintenanceroutes.js");
+app.use("/maintenance" , maintenanceRouter );
 
-const ScheduleCRoute = require("./routes/ScheduleCRoute.js");
-app.use("/shedulech",ScheduleCRoute);
+const maintenance1Router = require("./routes/Maintnance1routes.js");
+app.use("/maintenance1" , maintenance1Router );
 
-const ReportRoute = require("./routes/TrainerReportRoutes.js");
-app.use("/Report",ReportRoute);
+const packageRouter = require("./routes/packagesRoute.js");
+app.use("/package", packageRouter);
+
+const proPackageRouter = require("./routes/proPackageRoute.js");
+app.use("/proPackage", proPackageRouter);
+
+const userPackage = require("./routes/userPkgRoute.js");
+app.use("/userPkg", userPackage);
+
+const userProPackage = require("./routes/userProPkgRoute.js");
+app.use("/userProPkg", userProPackage);
+
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`)
+})
 
 
 
-
-app.listen(PORT,()=>{
-     console.log(`Server is up and runnng on port: ${PORT}`);
-});
