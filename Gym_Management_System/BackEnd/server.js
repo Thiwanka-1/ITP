1d3da1a899c66a1cca5d3482143fad20528c0da9
Thiwanka-1 <128372987+Thiwanka-1@users.noreply.github.com>
2024-04-16 17:24:49 +0530

    new

--- Gym_Management_System/BackEnd/server.js
+++ Gym_Management_System/BackEnd/server.js
@@ -1,8 +1,8 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv'); 
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const app = express();
 require("dotenv").config();
 
@@ -11,8 +11,8 @@ const PORT = process.env.PORT || 8070;
 app.use(cors());
 app.use(bodyParser.json());
 
-
 const URL = process.env.MONGODB_URL;
+
 
 mongoose.connect(URL,{
     useNewUrlParser: true,
@@ -22,9 +22,31 @@ mongoose.connect(URL,{
 
 
 const connection = mongoose.connection;
-connection.once('open', () => {
-    console.log("MongoDb Connection Success!");
-});
+connection.once('open' , () =>{
+    console.log("Mongodb Connection success!");
+
+})
+
+const AppoinmentRouter = require("./routes/AppoinmentRoutes.js");
+app.use("/appointment", AppoinmentRouter );
+
+const maintenanceRouter = require("./routes/Maintenanceroutes.js");
+app.use("/maintenance" , maintenanceRouter );
+
+const maintenance1Router = require("./routes/Maintnance1routes.js");
+app.use("/maintenance1" , maintenance1Router );
+
+const packageRouter = require("./routes/packagesRoute.js");
+app.use("/package", packageRouter);
+
+const proPackageRouter = require("./routes/proPackageRoute.js");
+app.use("/proPackage", proPackageRouter);
+
+const userPackage = require("./routes/userPkgRoute.js");
+app.use("/userPkg", userPackage);
+
+const userProPackage = require("./routes/userProPkgRoute.js");
+app.use("/userProPkg", userProPackage);
 
 const workoutrouter = require("./routes/WorkoutPlans.js");
 app.use("/workoutplan",workoutrouter);
@@ -41,9 +63,6 @@ app.use("/shedulech",ScheduleCRoute);
 const ReportRoute = require("./routes/TrainerReportRoutes.js");
 app.use("/Report",ReportRoute);
 
-
-
-
-app.listen(PORT,()=>{
-     console.log(`Server is up and runnng on port: ${PORT}`);
-});
+app.listen(PORT, () => {
+    console.log(`Server is up and running on port number: ${PORT}`)
+})
