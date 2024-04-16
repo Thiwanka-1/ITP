259935d38cef98d7345e469a9b7336f386aee2eb
Sarangi0611 <134938492+Sarangi0611@users.noreply.github.com>
2024-04-16 10:22:35 +0530

    add

--- GymManagementSystem/frontend/src/App.js
+++ GymManagementSystem/frontend/src/App.js
@@ -3,13 +3,14 @@ import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
 import TwodayWorkout from './pages/TwodayWorkout/TwodayWorkout';
 import Threedayworkoutplan from './pages/Threedayworkout/Threedayworkoutplan';
 import Workoutpage from './pages/Workoutpages/Workoutpage';  
-import Trainerdiplay from './pages/Trainerprofile/Trainerpro';
 import UpdateWorkout from './pages/UpdateWorkoutplan/Updateworkout';
 import Report from './pages/TrainerReport/TrainersReport';
 import Display from './pages/Trainerprofile/Trainerpro';
 import OneUpdate from './pages/OnedayUpdate/OnedayUpdate';
 import TwoUpdate from './pages/TwodayUpdate/TwodayUpdate';
 import ThreeUpdate from './pages/ThreedayUpdate/ThreedayUpdate';
+import TrainerRequest from './pages/TrainerRequest/TrainerRequest';
+import SheduleRequest from './pages/SheduleRequest/SheduleRequest'; 
 
 function App() {
   return (
@@ -19,13 +20,14 @@ function App() {
       <Route path='/twoadd' element={<TwodayWorkout/>}/>
       <Route path='/threeadd' element={<Threedayworkoutplan/>}/>
       <Route path='/wpage' element={<Workoutpage/>}/>
-      <Route path='/trainerdiplay' element={<Trainerdiplay/>}/>
       <Route path='/UpdateWorkout' element={<UpdateWorkout/>}/>
       <Route path='/Report' element={<Report/>}/>
       <Route path='/Trainers' element={<Display/>}/>
       <Route path='/update/oneday/:id' element={<OneUpdate/>}/>
       <Route path='/update/twoday/:id' element={<TwoUpdate/>}/>
       <Route path='/update/threeday/:id' element={<ThreeUpdate/>}/>
+      <Route path='/TrainerRequest' element={<TrainerRequest/>}/>
+      <Route path='/Shedule' element={<SheduleRequest/>}/>
     </Routes>
     </Router>
   );
